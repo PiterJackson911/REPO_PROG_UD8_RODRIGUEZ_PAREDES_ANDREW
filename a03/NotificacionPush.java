3d3a7e3dc4e0266459e292fa0c4af05f096ea848
ARP <andrewarp06@gmail.com>
2026-03-24 08:53:18 +0100

    Add files via upload

---
+++ a03/NotificacionPush.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package a03;
+
+/**
+ *
+ * @author ALUMNO
+ */
+public class NotificacionPush  implements Notificable {
+    @Override 
+    public void enviarNotificacion(String mensaje) {
+        System.out.println("Enviando por PUSH: " + mensaje);
+}
+}
